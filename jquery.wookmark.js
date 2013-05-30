2275aa930e7b8d0102f2fb3b90233516b5ab3e5a
Sebastian Helzle <sebastian@helzle.net>
2013-05-30 15:01:05 +0200

    Preparing new release

--- jquery.wookmark.js
+++ jquery.wookmark.js
@@ -3,8 +3,8 @@
   @name jquery.wookmark.js
   @author Christoph Ono (chri@sto.ph or @gbks)
   @author Sebastian Helzle (sebastian@helzle.net or @sebobo)
-  @version 1.2.0
-  @date 5/24/2013
+  @version 1.2.1
+  @date 5/30/2013
   @category jQuery plugin
   @copyright (c) 2009-2013 Christoph Ono (www.wookmark.com)
   @license Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
